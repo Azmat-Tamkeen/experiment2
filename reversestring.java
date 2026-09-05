7432c9c550f9ea394dc981cb542b72bb2c2b73d8
Azmat Tamkeen <azmatchand9581@gmail.com>
2026-09-05 11:36:34 +0530

    added reverse string.java

---
+++ reversestring.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class ReverseString {
+    public static void main(string args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String str = sc.nextLine();
+        String reversedStr = "";
+        for(int i = str.length() - 1; i >= 0; i--){
+            reversedStr += str.charAt(i);
+        }
+        System.out.println("Reversed string: " + reversedStr);
+    }
